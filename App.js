5bf883b0ba53fc562fb520cf70cc63e29551b81d
Basketball50 <144744097+Basketball50@users.noreply.github.com>
2025-08-29 21:29:41 -0400

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import AuthPage from './AuthPage';
 import Dashboard from './Dashboard';
 import Tracking from './Tracking';
-import SessionSummary from './SessionSummary'; // ✅ added
+import SessionSummary from './SessionSummary'; 
 
 function LandingPage() {
   return (
